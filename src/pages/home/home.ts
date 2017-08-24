09823733290f6f85f7308ca652fe6db737f7ac02
alikoganyan <alik.oganyan@gmail.com>
2017-08-24 18:31:04 +0400

    get address from provider and show on UI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SalonProvider } from '../../providers/salon/salon';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  salons = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private salonProvider: SalonProvider) {
 
+  }
+
+  ngOnInit() {
+    this.salons = this.salonProvider.getSalons();
+    console.log(this.salons);
   }
 
 }
--- src/providers/salon/salon.ts
+++ src/providers/salon/salon.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
@@ -10,9 +10,64 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class SalonProvider {
+  salons = [
+    {
+      id: 1,
+      img: "assets/images/salon_box/img1.jpg",
+      address: "Непокоренных, 10",
+      metroAddress: "Площадь мужества"
+    },
+    {
+      id: 2,
+      img: "assets/images/salon_box/img2.jpg",
+      address: "Большая Зеленина, 3",
+      metroAddress: "Чкаловская"
+    },
+    {
+      id: 3,
+      img: "assets/images/salon_box/img3.jpg",
+      address: "Александра Невского 21 к.2",
+      metroAddress: "Адмиралтейская"
+    },
+    {
+      id: 4,
+      img: "assets/images/salon_box/img4.jpg",
+      address: "Невский проспект, 23",
+      metroAddress: "Адмиралтейская"
+    },
+    {
+      id: 5,
+      img: "assets/images/salon_box/img5.jpg",
+      address: "Непокоренных, 10",
+      metroAddress: "Площадь мужества"
+    },
+    {
+      id: 5,
+      img: "assets/images/salon_box/img6.jpg",
+      address: "СНепокоренных, 10",
+      metroAddress: "Площадь мужества"
+    },
+    {
+      id: 5,
+      img: "assets/images/salon_box/img7.jpg",
+      address: "СНепокоренных, 10",
+      metroAddress: "Площадь мужества"
+    },
+
+  ];
 
   constructor(public http: Http) {
     console.log('Hello SalonProvider Provider');
+  }
+
+  getSalons() {
+    const headers = new Headers();
+    headers.set('Content-Type', 'application/json');
+    headers.set('Authorization', 'Basic ZW1haWxAZG9tYWluLmNvbToxMjM0NTY=');
+
+    // return this.http.get('http://avisits.com:8080/api/salons', {headers: headers})
+    // .map(response => response.json().data);
+    return this.salons;
   }
 
 }
